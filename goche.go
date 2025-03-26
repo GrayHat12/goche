631a2b6de78f35ad47a55c2da8ecadb73eed2240
GrayHat12 <grayhathacks10@gmail.com>
2025-03-26 13:23:27 +0530

    goche init

---
+++ goche.go
@@ -0,0 +1,47 @@
+package goche
+
+import (
+	"errors"
+	"sync"
+)
+
+type GenericFunction[T any] func() T
+
+type Cache[T any] struct {
+	store map[string]T
+	mux   sync.RWMutex
+	keys  []string
+	index int8
+	max   int8
+}
+
+func NewCache[T any](max int8) *Cache[T] {
+	return &Cache[T]{make(map[string]T), sync.RWMutex{}, []string{}, 0, max}
+}
+
+func (c *Cache[T]) removeKeyOnCurrentIndex() {
+	key_on_current_index := c.keys[c.index]
+	delete(c.store, key_on_current_index)
+}
+
+func (c *Cache[T]) Set(id string, value T) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.removeKeyOnCurrentIndex()
+	c.keys[c.index] = id
+	c.index = (c.index + 1) % c.max
+	c.store[id] = value
+}
+
+func (c *Cache[T]) Get(id string) (T, error) {
+	var none T
+	c.mux.RLock()
+	v, ok := c.store[id]
+	c.mux.RUnlock()
+
+	if !ok {
+		return none, errors.New("a value with given key not found")
+	}
+
+	return v, nil
+}
